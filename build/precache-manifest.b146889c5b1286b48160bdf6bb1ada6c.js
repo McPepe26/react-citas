5569222df0875cb49c3500a894cf67d03ec15e88
Andres Zamora <1830635@upt.edu.mx>
2021-12-13 18:42:41 -0600

    build

---
+++ build/precache-manifest.b146889c5b1286b48160bdf6bb1ada6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b125b2d41d4e257741d9434de4dd30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979cd4761cd8181f4e2a",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "a475d1670470b3dfe105",
+    "url": "/static/js/2.ad46ac13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad46ac13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979cd4761cd8181f4e2a",
+    "url": "/static/js/main.725970f3.chunk.js"
+  },
+  {
+    "revision": "8f7cc1bf3f0804bf445a",
+    "url": "/static/js/runtime-main.5666ba26.js"
+  }
+]);
